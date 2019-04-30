e7957d0341079afb9e1f72dd4ae08cffa6ad7333
SveinJH <43957538+SveinJH@users.noreply.github.com>
2019-04-30 19:44:37 +0200

    Work 30/4 3 hours

--- resources/js/script.js
+++ resources/js/script.js
@@ -12,7 +12,6 @@ $(document).ready(function() {
     });
 
 
-
     //SCROLL TO SECTIONS
     $('.js-scroll-to-hiw').click(function() {
         $('html, body').animate({scrollTop: $('.js-section-hiw').offset().top}, 1000);
@@ -48,10 +47,40 @@ $(document).ready(function() {
 
 
     //ANIMATIONS ON SCROLL
-    
+    $('.js-wp-1').waypoint(function(direction) {
+        $('.js-wp-1').addClass('animated rotateInDownLeft');
+    }, {
+        offset: '55%'
+    });
+
+    $('.js-wp-2').waypoint(function(direction) {
+        $('.js-wp-2').addClass('animated fadeInUp');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js-wp-3').waypoint(function(direction) {
+        $('.js-wp-3').addClass('animated fadeIn');
+    }, {
+        offset: '55%'
+    });
+
+    $('.js-wp-4').waypoint(function(direction) {
+        $('.js-wp-4').addClass('animated slideInUp');
+    }, {
+        offset: '60%'
+    });
+
+    $('.js-wp-5').waypoint(function(direction) {
+        $('.js-wp-5').addClass('animated bounceInUp');
+    }, {
+        offset: '45%'
+    });
 
 
-    //Calculate prices
+    //CALCULATE PRICES
+
+    var member = false;
 
     $('#days').on('change', function(e){
         calculate();
@@ -73,6 +102,21 @@ $(document).ready(function() {
         calculate();
     });
 
+    $('.memberButton').click(function() {
+        let number = $('#memberNumber').val();
+        
+
+        if(member) {
+            alert("Medlemsnummer allerede registrert!")
+        } else if(number.length === 8 && number.substring(0,3) == "EBB"){
+            alert("Medlem registrert!");
+            member = true;
+            calculate();
+            document.getElementById('memberNumber').disabled = true;
+        } else {
+            alert("Ugyldig medlemsnummer!");
+        }
+    });
 
 
     function calculate() {
@@ -82,7 +126,7 @@ $(document).ready(function() {
         cabin = $('#cabin').val();
         people = $('#peopleNumber').val();
         travel = $('#travel').val();
-        finalPrice = 1;
+        finalPrice = 800;
 
         summary = [days, month, cabin, people, travel];
 
@@ -95,13 +139,63 @@ $(document).ready(function() {
             finalPrice *= summary[i];
         }
 
-        $('.calculatedPrice').text(finalPrice);
+        if(member){
+            finalPrice *= .8;
+        }
+
+        $('.calculatedPrice').text(Math.floor(finalPrice));
     }
+
+
+
+    //CHANGE PICTURE ON ROUTE CHANGE
+    $('#travel').on('change', function(e) {
+        let value = $('#travel').val();
+
+        if(value == 1.05){
+            $('.order__img--selected').attr("src", "resources/img/travel-1.jpg");
+        } else if(value == 1.10) {
+            $('.order__img--selected').attr("src", "resources/img/travel-2.jpg");
+        } else {
+            $('.order__img--selected').attr("src", "resources/img/travel-3.jpg");
+        }
+    });
+    
+    //CHANGE TO CORRECT PICTURE AND TRAVEL ON SHIP BUTTONS
+    $('.js-book_SQ').click(function() {
+        $('#travel').val('1');
+        $('.order__img--selected').attr("src", "resources/img/travel-3.jpg");
+        calculate();
+    });
+
+    $('.js-book_CP').click(function() {
+        $('#travel').val('1.1');
+        $('.order__img--selected').attr("src", "resources/img/travel-1.jpg");
+        calculate();
+    });
+
+    $('.js-book_CS').click(function() {
+        $('#travel').val('1.05');
+        $('.order__img--selected').attr("src", "resources/img/travel-2.jpg");
+        calculate();
+    });
+
 });
 
-function submitForm() {
-    alert("hi");
-    let frm = document.getElementsByName('orderForm')[0];
+
+//MESSAGE SUBMIT
+function submitMessage() {
+    $('#nameLabel').val('');
+    $('#emailLabel').val('');
+    $('#messageLabel').val('');
+    alert("Your message has been sent! Thank you.");
     return false;
 }
 
+
+//ORDER SUBMIT
+function submitForm() {
+    $('#firstName').val('');
+    $('#emailLabel').val('');
+    $('#messageLabel').val('');
+}
